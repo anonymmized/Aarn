81f07981aab454de4afa0f102a03596dcaaef122
anonymmized <lih0deev@inbox.ru>
2026-01-19 22:20:40 +0300

    pwd were added

--- clifm.c
+++ clifm.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAXLINE 1024
+#define DIR_LEN 128
 
 int get_line(char *line, int lim) {
     int c, i = 0;
@@ -15,16 +17,30 @@ int get_line(char *line, int lim) {
     return i;
 }
 
+const char *print_workin(void) {
+    const char *pwd = getenv("PWD");
+    if (!pwd) {
+        fprintf(stderr, "PWD is not set\n");
+        return NULL;
+    }
+    return pwd;
+}
+
 int main() {
     printf("> ");
     char line[MAXLINE];
     int len;
     while ((len = get_line(line, MAXLINE)) > 0) {
         if (strcmp(line, "exit\n") == 0) {
-            printf("exit in progress...\n");
+            fprintf(stdout, "exit in progress...\n");
             break;
+        } else if (strcmp(line, "pwd\n") == 0) {
+            const char *pwd_v;
+            if ((pwd_v = print_workin()) != NULL) {
+                fprintf(stdout, "%s\n", pwd_v);
+            }
         }
-        printf("%s", line);
+        // printf("%s", line);
         
         printf("> ");
     }
