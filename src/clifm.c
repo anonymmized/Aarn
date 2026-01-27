4fd7243954594cf12f467e7a02da13a5d842451f
anonymmized <lih0deev@inbox.ru>
2026-01-27 20:58:50 +0300

    [remove] commented lines with old functions

--- src/clifm.c
+++ src/clifm.c
@@ -25,59 +25,6 @@
 #define LS_A 1
 
 int remove_dir(const char *name);
-/*
-enum Commands {CMD_exit, CMD_pwd, CMD_ls, CMD_cd, CMD_clear, CMD_cat, CMD_rm};
-typedef struct {
-    const char *name;
-    int id;
-} Cmd;
-static Cmd cmds[] = {
-    {"exit", CMD_exit},
-    {"pwd", CMD_pwd},
-    {"ls", CMD_ls},
-    {"cd", CMD_cd},
-    {"clear", CMD_clear},
-    {"cat", CMD_cat},
-    {"rm", CMD_rm}
-};
-
-typedef struct {
-    int files;
-    int dirs;
-} Count;
-
-char *skip_spaces(char *);
-
-Count dir_items(const char *dirname) {
-    Count c = {0, 0};
-
-    DIR *w_dir = opendir(dirname);
-    if (!w_dir) {
-        return c;
-    }
-
-    struct dirent *ent;
-    while ((ent = readdir(w_dir)) != NULL) {
-        if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0) continue;
-        if (ent->d_type == DT_DIR) c.dirs++;
-        else c.files++;
-    }
-    closedir(w_dir);
-    return c;
-}
-
-int get_line(char *line, int lim) {
-    int c, i = 0;
-    while (--lim > 0 && (c = getchar()) != EOF && c != '\n') {
-        line[i++] = c;
-    }
-    if (c == '\n') {
-        line[i++] = '\n';
-    }
-    line[i] = '\0';
-    return i;
-}
-*/
 
 const char *print_workin(void) {
     static char buf[PATH_MAX];
@@ -120,17 +67,6 @@ int list_wd(char *dir, int hidden) {
     closedir(w_dir);
     return 0;
 }
-/*
-char *skip_spaces(char *s) {
-    while (*s == ' ' || *s == '\t') s++;                                                 return s;
-}
-
-int dir_exists(const char *path) {
-    struct stat st;
-    return (stat(path, &st) == 0) && S_ISDIR(st.st_mode);
-}
-*/
-   
 int cmd_cd(const char *path) {
     if (!path || !*path || *path == ' ') path = getenv("HOME");
     if (!path) return 1;
@@ -141,42 +77,6 @@ int cmd_cd(const char *path) {
     }
     return 0;
 }
-/*
-int parse_line(char *line, char **argv, int max) {
-    int argc = 0;
-    line[strcspn(line, "\r\n")] = 0;
-
-    while (*line && argc < max - 1) {
-        while (*line == ' ' || *line == '\t') line++;
-        if (!*line) break;
-
-        argv[argc++] = line;
-        while (*line && *line != ' ' && *line != '\t') line++;
-        if (*line) *line++ = 0;
-    }
-    argv[argc] = NULL;
-    return argc;
-}
-
-int get_command_id(char *line) {
-    char *argv[16];
-    int arg_count = parse_line(line, argv, 16);
-    if (arg_count < 1) {
-        printf("\n");
-        return -10;
-    }
-    char *command = argv[0];
-    for (int i = 0; i < 7; i++) {
-        if (strcmp(command, cmds[i].name) == 0) return cmds[i].id;
-    }
-    return -1;
-}
-
-const char *return_last_dir(const char *workin) {
-    const char *last = strrchr(workin, '/');
-    return (last && *(last + 1)) ? last + 1 : workin;
-}
-*/
 
 int cat_file(const char *filename) {
     FILE *fp = fopen(filename, "r");
