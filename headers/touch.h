3f3407aa20bb2f075c26de0c3370bfa1e8599dd3
anonymmized <lih0deev@inbox.ru>
2026-02-03 15:40:47 +0300

    [add] handling -a and -m for touch

--- headers/touch.h
+++ headers/touch.h
@@ -1,6 +1,6 @@
 #ifndef TOUCH_H
 #define TOUCH_H
 
-int touch_file(const char *path);
+int touch_file(const char *path, int atime, int mtime);
 
 #endif
--- src/touch.c
+++ src/touch.c
@@ -1,22 +1,40 @@
 #include "../headers/touch.h"
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-int touch_file(const char *path) {
+int touch_file(const char *path, int atime, int mtime) {
     int existed = (access(path, F_OK) == 0);
-    FILE *fp = fopen(path, "w");
-    if (!fp) {
-        perror("fopen");
-        return 1;
-    } else {
-        if (!existed) {
-            printf("file was created\n");
-        } 
-        else {
-            printf("file was overwritten\n");
+    if (!existed) {
+        int fd = open(path, O_CREAT | O_WRONLY, 0644);
+        if (fd == -1) {
+            perror("open");
+            return 1;
         }
-        fclose(fp);
-        return 0;
+        close(fd);
     }
+
+    struct timespec times[2];
+    times[0].tv_nsec = UTIME_NOW;
+    times[1].tv_nsec = UTIME_NOW;
+    if (atime == 1 && mtime == 0) {
+        times[0].tv_sec = time(NULL);
+        times[0].tv_nsec = 0;
+        times[1].tv_nsec = UTIME_OMIT;
+    }
+    if (mtime == 1 && atime == 0) {
+        times[0].tv_nsec = UTIME_OMIT;
+        times[1].tv_sec = time(NULL);
+        times[1].tv_nsec = 0;
+    }
+
+    if (utimensat(AT_FDCWD, path, times, 0) == -1) {
+        perror("utimensat");
+        return 1;
+    }
+
+    return 0;
 }
