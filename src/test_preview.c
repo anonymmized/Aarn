62371695f581934b6e2d85cce4f0bb3e2a2f1f71
anonymmized <lih0deev@inbox.ru>
2026-02-07 18:25:43 +0300

    [add] printing file content

--- src/test_preview.c
+++ src/test_preview.c
@@ -13,6 +13,23 @@ void redraw(char **f_list, int len, int target, int chose);
 
 struct termios orig;
 
+int cat_file(char *filename) {
+    FILE *fp = fopen(filename, "r");
+    if (!fp) {
+        perror("fp");
+        return 1;
+    }
+    char buf[1024];
+    while (fgets(buf, sizeof(buf), fp)) fputs(buf, stdout);
+    if (ferror(fp)) {
+        perror("fgets");
+        fclose(fp);
+        return 1;
+    }
+    fclose(fp);
+    return 0;
+}
+
 int check_dir(char *filename) {
     struct stat buf;
     if (stat(filename, &buf) != 0) {
@@ -54,6 +71,9 @@ void input_monitor(char **f_list, int len, int target) {
             chose = 0;
             continue;
         }
+        if (c == 'q') {
+            return;
+        }
         if (c == '\x1b') {
             char seq[2];
             if (read(STDIN_FILENO, &seq[0], 1) <= 0) continue;
@@ -76,7 +96,7 @@ void input_monitor(char **f_list, int len, int target) {
                 printf("Стрелка вправо\n");
             if (seq[1] == 'D')
                 printf("Стрелка влево\n");
-             
+            
         }
     }
 }
@@ -86,21 +106,39 @@ void redraw(char **f_list, int len, int target, int chose) {
     if (target > len) {
         return;
     }
+    char target_name[1024];
     for (int i = 0; i < len; i++) {
+        printf("\033[%d;1H", i + 1);
         char *new_s = strrchr(f_list[i], '/') + 1;
         if (i == target) {
             if (chose == 1) {
                 if (check_dir(f_list[i]) == 1) {
-                    printf(ORANGE "%s - file\n" ESC, new_s);
+                    printf(ORANGE "\t%s\n\n" ESC, new_s);
+                    strcpy(target_name, f_list[i]);
                 } else if (check_dir(f_list[i]) == 2) {
-                    printf(ORANGE "%s - directory\n" ESC, new_s);
+                    printf(ORANGE "\t%s\n\n" ESC, new_s);
                 }
             } else {
-                printf(ORANGE "%s\n" ESC, new_s);
+                printf(ORANGE "\t%s\n\n" ESC, new_s);
             }
         } else {
-            printf("%s\n", new_s);
+            printf("\t%s\n\n", new_s);
         }
+    }
+    if (chose == 1) { 
+        FILE *fp = fopen(target_name, "r");
+        if (!fp) {
+            perror("fopen");
+            return;
+        }
+        char line[1024];
+        int row = 1;
+        while (fgets(line, sizeof(line), fp) && row < 40) {
+            printf("\033[%d;40H", row);
+            printf("%s", line);
+            row++;
+        }
+        fclose(fp);
     }
     fflush(stdout);
 }
@@ -126,14 +164,15 @@ int list(char *dir, char **f_list) {
 
 
 int main() {
+    printf("\033[?25l");
     char *f_list[100];
-    int items_count = list("..", f_list);
+    int items_count = list(".", f_list);
     printf("Current dir: %d\n", items_count);
     enable_raw();
-    while (1) {
-        input_monitor(f_list, items_count, 11);
-    }
+    input_monitor(f_list, items_count, 11);
+    
     disable_raw();
+    printf("\033[?25h");
     for (int i = 0; i < items_count; i++) {
         free(f_list[i]);
     }
