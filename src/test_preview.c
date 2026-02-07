e7ff3e4057615498bab769a33e915972273ca6ac
anonymmized <lih0deev@inbox.ru>
2026-02-07 13:41:07 +0300

    [add] dir & files types print

--- src/test_preview.c
+++ src/test_preview.c
@@ -4,13 +4,31 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <termios.h>
+#include <sys/stat.h>
 
 #define ESC    "\033[0m"
 #define ORANGE "\033[33m"
 
-void redraw(char **f_list, int len, int target);
+void redraw(char **f_list, int len, int target, int chose);
 
 struct termios orig;
+
+int check_dir(char *filename) {
+    struct stat buf;
+    char fullpath[100];
+    snprintf(fullpath, sizeof(fullpath), "../%s", filename);
+    if (stat(fullpath, &buf) != 0) {
+        perror("stat");
+        return -1;
+    }
+    if (S_ISREG(buf.st_mode)) {
+        return 1; // file
+    } else if (S_ISDIR(buf.st_mode)) {
+        return 2; // dir
+    } else {
+        return -1; // not dir & not file
+    }
+}
 
 void enable_raw(void) {
     tcgetattr(STDIN_FILENO, &orig);
@@ -25,10 +43,17 @@ void disable_raw(void) {
 
 void input_monitor(char **f_list, int len, int target) {
     int index = 0;
-    redraw(f_list, len, index);
+    int chose = 0;
+    redraw(f_list, len, index, chose);
     while (1) {
         char c;
         if (read(STDIN_FILENO, &c, 1) <= 0) {
+            continue;
+        }
+        if (c == '\n') {
+            chose = 1;
+            redraw(f_list, len, index, chose);
+            chose = 0;
             continue;
         }
         if (c == '\x1b') {
@@ -38,14 +63,14 @@ void input_monitor(char **f_list, int len, int target) {
             if (seq[1] == 'A') {
                 if (index != 0) {
                     index -= 1;
-                    redraw(f_list, len, index);
+                    redraw(f_list, len, index, chose);
                 }
             }
             
             if (seq[1] == 'B') {
                 if (index < len) {
                     index += 1;
-                    redraw(f_list, len, index);
+                    redraw(f_list, len, index, chose);
                 }
             }
             
@@ -58,14 +83,22 @@ void input_monitor(char **f_list, int len, int target) {
     }
 }
 
-void redraw(char **f_list, int len, int target) {
+void redraw(char **f_list, int len, int target, int chose) {
     printf("\033[H\033[J");
     if (target > len) {
         return;
     }
     for (int i = 0; i < len; i++) {
         if (i == target) {
-            printf(ORANGE "%s\n" ESC, f_list[i]);
+            if (chose == 1) {
+                if (check_dir(f_list[i]) == 1) {
+                    printf(ORANGE "%s - file\n" ESC, f_list[i]);
+                } else if (check_dir(f_list[i]) == 2) {
+                    printf(ORANGE "%s - directory\n" ESC, f_list[i]);
+                }
+            } else {
+                printf(ORANGE "%s\n" ESC, f_list[i]);
+            }
         } else {
             printf("%s\n", f_list[i]);
         }
@@ -93,7 +126,7 @@ int list(char *dir, char **f_list) {
 
 int main() {
     char *f_list[100];
-    int items_count = list(".", f_list);
+    int items_count = list("..", f_list);
     printf("Current dir: %d\n", items_count);
     enable_raw();
     while (1) {
