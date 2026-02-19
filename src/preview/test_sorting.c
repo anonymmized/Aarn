a40d5acbd608b5417671012c75432b910b63daa9
anonymmized <lih0deev@inbox.ru>
2026-02-19 18:03:27 +0300

    [add] sorting any type

--- src/preview/test_sorting.c
+++ src/preview/test_sorting.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
+int file_cmp(const void *a, const void *b);
+
 int list(const char *path, char **f_list) {
     DIR *wdir = opendir(path);
     if (!wdir) {
@@ -23,62 +25,70 @@ int list(const char *path, char **f_list) {
     return items_count;
 }
 
-void swap(int *a, int *b) {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
+void swap(void *a, void *b, size_t size) {
+    char tmp[size];
+    memcpy(tmp, a, size);
+    memcpy(a, b, size);
+    memcpy(b, tmp, size);
 }
 
-void quick_sort(int *arr, int left, int right) {
-    int i = left, j = right;
-    int pivot = arr[(left + right) / 2];
+void quick_sort(void *base, int left, int right, size_t size, int (*cmp)(const void *, const void *)) {
+    char *arr = (char *)base;
+
+    int i = left;
+    int j = right;
+    char pivot[size];
+
+    memcpy(pivot, arr + ((right + left) / 2) * size, size);
     while (i <= j) {
-        while (arr[i] < pivot) i++;
-        while (arr[j] > pivot) j--;
+        while (cmp(arr + i * size, pivot) < 0) i++;
+        while (cmp(arr + j * size, pivot) > 0) j--;
+
         if (i <= j) {
-            swap(&arr[i], &arr[j]);
+            swap(arr + i * size, arr + j * size, size);
             i++;
             j--;
         }
     }
     if (left < j) {
-        swap(arr, left, j);
+        quick_sort(base, left, j, size, cmp);
     }
-    if (i < right) {
-        swap(arr, i, right);
+    if (right > i) {
+        quick_sort(base, i, right, size, cmp);
     }
 }
 
-int comparator(char *path1, char *path2) {
-    struct stat st1;
-    struct stat st2;
-    if (stat(path1, &st1) == 0 && stat(path2, &st2) == 0) {
-        if (S_ISDIR(st1.st_mode) && S_ISDIR(st2.st_mode)) {
-            char *p1 = path1;
-            char *p2 = path2;
-            while (*p1 != '\0' || *p2 != '\0') {
-                if ((int)*p1 > (int)*p2) return -1;
-                else if ((int)*p1 < (int)*p2) return 1;
-                else {
-                    p1++;
-                    p2++;
-                }
-            }
-        } 
-        else if (S_ISDIR(st1.st_mode) && !S_ISDIR(st2.st_mode)) return 1;
-        else if (!S_ISDIR(st1.st_mode) && S_ISDIR(st2.st_mode)) return -1;
-        else {
-            char *p1 = path1;
-            char *p2 = path2;
-            while (*p1 != '\0' || *p2 != '\0') {
-                if ((int)*p1 > (int)*p2) return -1;
-                else if ((int)*p1 < (int)*p2) return 1;
-                else {
-                    p1++;
-                    p2++;
-                }
-            }
-        }
+int file_cmp(const void *a, const void *b) {
+    const char *path1 = *(const char**)a;
+    const char *path2 = *(const char**)b;
+
+    struct stat st1, st2;
+
+    if (stat(path1, &st1) != 0 || stat(path2, &st2) != 0) {
+        return 0;
+    }
+    int dir1 = S_ISDIR(st1.st_mode);
+    int dir2 = S_ISDIR(st2.st_mode);
+
+    if (dir1 && !dir2) return -1;
+    if (dir2 && !dir1) return 1;
+
+    const char *name1 = strrchr(path1, '/');
+    const char *name2 = strrchr(path2, '/');
+
+    name1 = name1 ? name1 + 1 : path1;
+    name2 = name2 ? name2 + 1 : path2;
+
+    return strcmp(name1, name2);
+}
+
+int main() {
+    const char path[] = ".";
+    char *f_list[1024];
+    int n = list(path, f_list);
+    quick_sort(f_list, 0, n-1, sizeof(char *), file_cmp);
+    for (int i = 0; i < n; i++) {
+        printf("%s\n", f_list[i]);
     }
     return 0;
 }
